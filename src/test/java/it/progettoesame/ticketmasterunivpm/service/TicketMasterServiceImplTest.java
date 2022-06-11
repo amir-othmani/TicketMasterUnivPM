7978906b6f86bb15cd16d3c2d0c6e348d5313575
amir-othmani <amir.othmani@hotmail.it>
2022-06-11 14:23:52 +0200

    Testato il service

--- src/test/java/it/progettoesame/ticketmasterunivpm/service/TicketMasterServiceImplTest.java
+++ src/test/java/it/progettoesame/ticketmasterunivpm/service/TicketMasterServiceImplTest.java
@@ -1,39 +1,27 @@
 package it.progettoesame.ticketmasterunivpm.service;
 
-import it.progettoesame.ticketmasterunivpm.filter.EventsFilter;
-import it.progettoesame.ticketmasterunivpm.parser.EventsParser;
+import it.progettoesame.ticketmasterunivpm.model.Event;
 import it.progettoesame.ticketmasterunivpm.stats.EventsStats;
 import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TicketMasterServiceImplTest {
 
-    private TicketMasterServiceImpl service = new TicketMasterServiceImpl();
-    final private JSONObject events = new JSONObject();
-    final private JSONObject allStats = new JSONObject();
-    final private JSONArray statsArray = new JSONArray();
+    final private TicketMasterServiceImpl service = new TicketMasterServiceImpl();
     final private String[] supportedEventsParam = {"countryCode", "city", "local_date", "segment", "genre", "subgenre"};
     final private String[] supportedCountries = {"AL", "AT", "BE", "BG", "CH", "CY", "CZ", "DE", "DK", "EE", "ES", "FO",
             "FI", "FR", "GB", "GR", "HR", "HU", "IE", "IS", "IT", "LT", "LU", "MC", "ME", "MT", "ND", "NL", "NO", "PL",
             "PT", "RO", "RS", "SE", "SK", "SI", "TR", "UA"};
-    final private EventsParser eventsParser = new EventsParser();
-    final private EventsFilter eventsFilter = new EventsFilter();
-    final private EventsStats eventsStats = new EventsStats();
-    private String currentCountry = " ";
 
-    @BeforeAll
-    void setUp() throws Exception {
-
-    }
 
     @Test
-    void areSupportedParam() {
+    void areSupportedParamTest() {
         HashMap<String, String> param = new HashMap<>();
         param.put("countryCode", "DE");
         param.put("city", "Hannover");
@@ -48,24 +36,73 @@ class TicketMasterServiceImplTest {
             value = true;
         else
             value = false;
-        assertEquals(value, false);
+        assertFalse(value);
     }
 
     @Test
-    void isSupportedCountry() {
+    void isSupportedCountryTest() {
         String country = "LT";
         boolean value = false;
         for (String c: supportedCountries)
-            if (c.equals(country))
+            if (c.equals(country)) {
                 value = true;
-        assertEquals(value, true);
+                break;
+            }
+        assertTrue(value);
     }
 
     @Test
-    void getEvents() {
+    void getEventsTest() {
+        HashMap<String, String> param = new HashMap<>();
+        param.put("countryCode", "IT");
+        assertTrue(service.getEvents(param).containsKey("events_not_found"));
     }
 
     @Test
     void getStats() {
+        EventsStats eventsStats = new EventsStats();
+        JSONArray stats = new JSONArray();
+        ArrayList<Event> events1 = new ArrayList<>();
+        ArrayList<Event> events2 = new ArrayList<>();
+        HashMap<String, String> param1 = new HashMap<>();
+        HashMap<String, String> param2 = new HashMap<>();
+        param1.put("city", "Frankfurt am Main");
+        param2.put("city", "Lemgo");
+        events1.add(new Event(
+                "Monster Jam 2023 - LOGEN",
+                "Z698xZC2Z17uabs",
+                "https://www.ticketmaster.de/event/monster-jam-2023-logen-tickets/349631?language=en-us",
+                "Germany",
+                "Frankfurt am Main",
+                LocalDate.parse("2023-07-22"),
+                "Sports",
+                "Miscellaneous",
+                "Miscellaneous"
+        ));
+        events2.add(new Event(
+                "TBV Lemgo Lippe - Handball Sport Verein Hamburg",
+                "Z698xZC2Z178-FN",
+                "https://www.ticketmaster.de/event/tbv-lemgo-lippe-handball-sport-verein-hamburg-tickets/421223?language=en-us",
+                "Germany",
+                "Lemgo",
+                LocalDate.parse("2022-06-12"),
+                "Sports",
+                "Handball",
+                "Handball"
+        ));
+        events1.add(new Event(
+                "KALEO",
+                "Z698xZC2Z178-eo",
+                "https://www.ticketmaster.de/event/kaleo-tickets/421091?language=en-us",
+                "Germany",
+                "Frankfurt am Main",
+                LocalDate.parse("2022-06-13"),
+                "Music",
+                "Rock",
+                "Pop"
+        ));
+        stats.add(eventsStats.statsPerWeek(events1, param1));
+        stats.add(eventsStats.statsPerWeek(events2, param2));
+        assertEquals(stats.size(), 2);
     }
 }
